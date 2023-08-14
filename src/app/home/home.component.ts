2b5e5441674194eb52f82c6ec05fba81380b6d12
Mohamed Langi <mlbc95@gmail.com>
2023-08-14 14:43:58 -0500

    update home page style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
+declare const sal: () => any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  ngOnInit() {
+    sal()
+  }
 }
